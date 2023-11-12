ae912005ba19e2ca3d3b3dbdecf191766b1c9bdc
Phillip <phetdi@hotmail.com>
2023-11-12 14:19:36 +0200

    the 1st c++ practice code

---
+++ FirstCplusplusCode.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std
+
+int main() {
+    cout << "Wellcome to African Football Leaque!";
+    cout << "The 1st final match of the AFL is hosted in South Africa";
+    cout << "it is Mamelodi Soundowns VS Wydad Casablanca";
+    cout << "it is South Africa VS Moroco"
+    return 0;
+}
