b0422579d9f653944c54a8fec79abdef2788210f
Phillip <phetdi@hotmail.com>
2023-11-12 21:55:35 +0200

    made changes to the code

--- FirstCplusplusCode.cpp
+++ FirstCplusplusCode.cpp
@@ -3,8 +3,9 @@ using namespace std
 
 int main() {
     cout << "Wellcome to African Football Leaque!";
-    cout << "The 1st final match of the AFL is hosted in South Africa";
-    cout << "it is Mamelodi Soundowns VS Wydad Casablanca";
-    cout << "it is South Africa VS Moroco"
+    cout << "The 1st final match of the AFL is hosted in South Africa \n";
+    cout << "it is Mamelodi Soundowns VS Wydad Casablanca \n" ;
+    cout << "it is South Africa VS Moroco \n" ;
+    cout << "And South Africa won Congragulations to Mamelodi Sundowns";
     return 0;
 }
