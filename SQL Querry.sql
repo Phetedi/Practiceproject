92149a33f56a96959d68ea709fcba3665b5ccaaf
Phetedi <150651930+Phetedi@users.noreply.github.com>
2023-11-13 09:33:12 +0200

    Update SQL Querry.sql
    added another select code

--- SQL Querry.sql
+++ SQL Querry.sql
@@ -1,5 +1,11 @@
 Use FAM
 
-SELECT name, age, count(*) as highage FROM moleke.db
+SELECT name, age, count(*) as highage 
+FROM moleke.db
 where age = 100
 order by name;
+
+
+SELECT top 100
+FROM moleke.db
+where age => 37
